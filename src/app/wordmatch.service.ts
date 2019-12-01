754eb1758a723b6b26082fe6918d363d8a9353a5
Sachin Pawar <sachin.pawar@neebal.com>
2019-12-01 13:18:31 +0530

    changed api service endpoint

--- src/app/wordmatch.service.ts
+++ src/app/wordmatch.service.ts
@@ -9,22 +9,24 @@ import { State } from './state';
 })
 export class WordmatchService {
 
+  private baseUrl="https://wordsmatch-backend.herokuapp.com/";
+
   constructor(private httpClient: HttpClient) { }
 
   public checkIfWordIsValid(word : string) : Observable<Response> {
-    return this.httpClient.get<Response>("http://127.0.0.1:5000/word/validate?word="+word);
+    return this.httpClient.get<Response>(this.baseUrl+"word/validate?word="+word);
   }
 
   public startNewGame() : Observable<State> {
-    return this.httpClient.get<State>("http://127.0.0.1:5000/word/new");
+    return this.httpClient.get<State>(this.baseUrl+"word/new");
   }
 
   public getGameState() : Observable<State> {
-    return this.httpClient.get<State>("http://127.0.0.1:5000/word/state");
+    return this.httpClient.get<State>(this.baseUrl+"word/state");
   }
 
   public gameOver() : Observable<State> {
-    return this.httpClient.get<State>("http://127.0.0.1:5000/word/end");
+    return this.httpClient.get<State>(this.baseUrl+"word/end");
   }
 
 
